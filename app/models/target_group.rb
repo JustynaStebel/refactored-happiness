58d15008a4fbd3cb63965a040f5269fef48ed296
JustynaStebel <justyna.stebel@gmail.com>
2017-02-06 12:34:17 +0100

    Add has one relation with itself

--- app/models/target_group.rb
+++ app/models/target_group.rb
@@ -2,6 +2,7 @@ class TargetGroup < ActiveRecord::Base
   belongs_to :panel_provider
   has_many :countries_target_groups
   has_many :countries, through: :countries_target_groups
+  has_one :parent, class_name: "TargetGroup", foreign_key: :parent_id
 
   validates_presence_of :name, :external_id, :secret_code
 end
