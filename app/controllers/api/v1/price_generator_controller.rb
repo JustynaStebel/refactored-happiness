6aba9f66cbd8a9faa0ed3cfcad80dbb03dbb4e76
JustynaStebel <justyna.stebel@gmail.com>
2017-02-07 15:46:33 +0100

    Pass params

--- app/controllers/api/v1/price_generator_controller.rb
+++ app/controllers/api/v1/price_generator_controller.rb
@@ -2,20 +2,19 @@ class Api::V1::PriceGeneratorController < Api::V1::BaseController
   before_action :get_country
 
   def evaluate_target
-    country_code = @country.country_code
+    target_group_id = @country.target_groups.find_by(id: params[:target_group_id])
+    location_ids = params[:locations].map { |loc| loc["id"] }
+    locations = @country.location_group.locations.find_by(location_ids)
     panel_id = @country.panel_provider_id
     render json: { price: get_panel(panel_id).perform }, status: :ok
   end
 
   private
   def get_country
-    @country = Country.find(params[:id])
+    @country = Country.find_by(country_code: params[:country_code])
   end
 
   def get_panel(panel_id)
     { 1 => P1PriceGenerator, 2 => P2PriceGenerator, 3 => P3PriceGenerator }.fetch(panel_id)
-  end
-
-  def price_params
   end
 end
